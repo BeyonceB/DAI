e50e5f3a5cb67db1487e521b2d5d8370f895c707
= <bbrowne14@my.bcit.ca>
2022-09-26 16:07:27 -0700

    commit

--- dai_web_ex/pages/index.js
+++ dai_web_ex/pages/index.js
@@ -1,10 +1,62 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { Loader, Button } from 'semantic-ui-react'
+import LoginForm from '../comps/LoginForm';
+
+//the router and states are mostly used in the page for data/information state managements
+import {useRouter} from 'next/router';
+import {useState} from 'react';
 
 export default function Home() {
+
+  const r = useRouter();
+
+  const [loginState, setLoginState] = useState("before")
+
+  var c = "blue";
+  var btn_txt = "Click Me"
+
+  // if(loginState === "before"){
+  //   c = "blue";
+  //   btn_txt = "Login"
+  // }
+
+  // if(loginState === "during"){
+  //   c = "green";
+  //   btn_txt = "Logging In"
+  // }
+
+  // if(loginState === "after"){
+  //   c = "grey";
+  //   btn_txt = "Success!"
+  // }
+
+  const Login = async () => {
+    setLoginState("during");
+
+    //it's going to take time
+    //mock of time passing -> this code is useless, it doesn't do anything
+
+    await new Promise(resolve=>setTimeout(resolve, 2000));
+
+    setLoginState("after");
+
+    //it's going to stall for 1-2 seconds
+    //mock of time passing -> this code is useless, it doesn't do anything
+    await new Promise(resolve=>setTimeout(resolve, 2000));
+
+    r.push("/dashboard");
+  }
+  
   return (
-    <div> hii
+    <div>
+      {/* <Button color={c} onClick={()=>Login()}>
+        {btn_txt}
+        {loginState === "during" && <Loader active />}
+      </Button> */}
+      <LoginForm 
+      loginState={loginState} 
+      onLoginClick={()=>Login()}
+      />
     </div>
   )
-  }
+
+}
